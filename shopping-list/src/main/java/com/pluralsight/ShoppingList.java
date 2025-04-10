ab79a839c9d9ab78bee68112d2284018b87e63df
Breyelle Bradley <breyellebradley@gmail.compwdgit config --global user.email breyellebradley@gmail.com>
2025-04-10 11:35:12 -0500

    completed shopping list app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("eggs");
+        System.out.println("flour");
+        System.out.println("coconut cream");
+        System.out.println("butter");
+        System.out.println("chickpeas");
+        System.out.println("naan");
+        System.out.println("tomato paste");
+        System.out.println("cheese");
+        System.out.println("spices");
+        System.out.println("rice");
+    }
+    
+    
+    
+}
